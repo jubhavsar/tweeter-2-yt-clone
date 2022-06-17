506e85359853a9b5e9a86baa09ad9cd436d0cf01
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 09:06:13 -0400

    customize color using utility class by config tailwind

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 function SidebarRow({Icon, title}: Props) {
   return (
-    <div className="flex items-center px-3 py-4 space-x-2 transition-all duration-200 rounded-full cursor-pointer group hover:bg-gray-100">
+    <div className="flex items-center px-3 py-4 space-x-2 transition-all duration-200 rounded-full cursor-pointer max-w-fit group hover:bg-gray-100">
       <Icon className='w-6 h-6'/>
-      <p className="group-hover:text-blue-300 ">{title}</p>
+      <p className="group-hover:text-tweeter ">{title}</p>
     </div>
   )
 }
