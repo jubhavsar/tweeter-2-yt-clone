48448da160c4f8c8260d3243170bd407a0abb347
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 19:54:44 -0400

    styled TweetBox component

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -9,23 +9,24 @@ import {
 
 function TweetBox() {
   return (
-    <div>
+    <div className='flex p-5 space-x-2'>
       <img className='object-cover mt-4 rounded-full h-14 w-14' src="https://links.papareact.com/gll" alt="" />
-      <div>
-        <form>
+      <div className='flex items-center flex-1 pl-2'>
+        <form className='flex flex-col flex-1'>
           <input type="text" placeholder="What's Happening"/>
-          <div>
-            <div className='flex space-x-2 text-tweeter'>
+          <div className='flex items-center'>
+            <div className='flex flex-1 space-x-2 text-tweeter'>
               <PhotographIcon className='w-5 h-5'/>
               <SearchCircleIcon className='w-5 h-5'/>
               <EmojiHappyIcon className='w-5 h-5'/>
               <CalendarIcon className='w-5 h-5'/>
               <LocationMarkerIcon className='w-5 h-5'/>
             </div>
-          </div>
-          <button className='px-5 py-2 font-bold text-white rounded-full bg-tweeter'>
+            <button className='px-5 py-2 font-bold text-white rounded-full bg-tweeter'>
             Tweet
           </button>
+          </div>
+          
         </form>
       </div>
     </div>
