9baa434772eadb2ee13fc1d0a455fc81ecdd22e1
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 19:22:08 -0400

    structured TweetBox component

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -4,6 +4,17 @@ function TweetBox() {
   return (
     <div>
       <img className='object-cover mt-4 rounded-full h-14 w-14' src="https://links.papareact.com/gll" alt="" />
+      <div>
+        <form>
+          <input type="text" placeholder="What's Happening" />
+          <div>
+            <div>{/* Icons */}</div>
+          </div>
+          <button>
+            Tweet
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
