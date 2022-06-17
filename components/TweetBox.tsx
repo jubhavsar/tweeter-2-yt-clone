e3a7590817d900984bf5f36322e309ce1a0cb98d
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 19:12:05 -0400

    styled image

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TweetBox() {
   return (
     <div>
-      <img src="https://links.papareact.com/gll" alt="" />
+      <img className='object-cover mt-4 rounded-full h-14 w-14' src="https://links.papareact.com/gll" alt="" />
     </div>
   )
 }
