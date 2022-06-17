beb0d079f4581e735c534f97cf09c5d581e1f34a
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 10:29:19 -0400

    install react twitter embed package and import it

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 import {
   SearchIcon
-} from '@heroicons/react/solid'
+} from '@heroicons/react/solid';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 function Widgets() {
   return (
-    <div>
-      <SearchIcon />
-      <input type="text"  placeholder='Search Twitter'/>
+    <div className='px-2 mt-2'>
+    <div className='flex items-center p-3 mt-2 space-x-2 bg-gray-100 rounded-full'>
+      <SearchIcon className='w-5 h-5 text-gray-400'/>
+      <input type="text" 
+             placeholder='Search Twitter'
+             className='flex-1 bg-transparent outline-none'/>
+    </div>
+    <TwitterTimelineEmbed
+      sourceType="profile"
+      screenName="saurabhnemade"
+       options={{height: 400}}
+    />
     </div>
   )
 }
