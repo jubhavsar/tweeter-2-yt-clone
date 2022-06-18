15902eff903336cd761f8a54e989d1a86b4dcb06
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-17 20:00:35 -0400

    styled input field

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -13,7 +13,10 @@ function TweetBox() {
       <img className='object-cover mt-4 rounded-full h-14 w-14' src="https://links.papareact.com/gll" alt="" />
       <div className='flex items-center flex-1 pl-2'>
         <form className='flex flex-col flex-1'>
-          <input type="text" placeholder="What's Happening"/>
+          <input type="text" 
+                 placeholder="What's Happening"
+                 className='w-full h-24 text-xl outline-none placeholder:text-xl'
+                 />
           <div className='flex items-center'>
             <div className='flex flex-1 space-x-2 text-tweeter'>
               <PhotographIcon className='w-5 h-5'/>
