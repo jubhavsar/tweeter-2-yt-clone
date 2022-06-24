a39a4fa9c9598c439198f179111c3703ce6cfb52
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-24 18:29:25 -0400

    implement fetchTweets utility function for frontend

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -20,7 +20,6 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const tweets: Tweet[] = await sanityClient.fetch(feedQuery)
-  console.log(tweets);
   
   res.status(200).json({ tweets })
 }
