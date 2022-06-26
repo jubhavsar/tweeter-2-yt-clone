ac65a60ae9e44110e124eaaa240f90e1243e10d7
jubhavsar <juhi_bhavsar@yahoo.com>
2022-06-25 23:27:23 -0400

    declare type defination for comment

--- typings.d.ts
+++ typings.d.ts
@@ -14,3 +14,22 @@ export type TweetBody = {
   image?: string
 
 }
+
+export type CommentBody = {
+  comment: string
+  username: string
+  profileImg: string
+  tweetId: string
+}
+
+export interface Comment extends CommentBody {
+  _id: string
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  _type: 'comment'
+  tweet: {
+    _ref: string
+    _type: 'reference'
+  }
+}
